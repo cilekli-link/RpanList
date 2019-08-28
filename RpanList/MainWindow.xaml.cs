47728a8ed142fe2c9f1325d6c1f11b85a74deb5b
ağl <al@al.al>
2019-08-29 02:21:37 +0300

    Add "broken pan" error message, and more try/catches

--- RpanList/MainWindow.xaml.cs
+++ RpanList/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using Newtonsoft.Json;
 using RpanList.Classes;
 using System;
+using System.ComponentModel;
 using System.Net;
 using System.Net.Http;
 using System.Windows;
+using System.Windows.Media;
 
 namespace RpanList
 {
@@ -12,58 +14,131 @@ namespace RpanList
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Set to true after clicking refresh on RpanDown grid
+        bool isRefreshing;
+        // Rotation of broken pan in RpanDown grid
+        double panRotation = 0;
+
         int streams = 0;
         int views = 0;
+
         public MainWindow()
         {
             InitializeComponent();
-            ApiResponse apiResponse;
+            parseResponse(grabResponse());
+        }
+
+        ApiResponse grabResponse()
+        {
             HttpClientHandler handler = new HttpClientHandler()
             {
                 AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
             };
-            using (var client = new HttpClient(handler))
+            var client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://strapi.reddit.com");
+            HttpResponseMessage response = new HttpResponseMessage();
+            try
             {
-                client.BaseAddress = new Uri("https://strapi.reddit.com");
-                HttpResponseMessage response = new HttpResponseMessage();
-                try
-                {
-                    response = client.GetAsync("videos/seed/360").Result;
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Could not connect to RPAN API.");
-                    return;
-                }
-
-                string str = response.Content.ReadAsStringAsync().Result;
-                apiResponse = JsonConvert.DeserializeObject<ApiResponse>(str);
-
-                if (apiResponse.status == "success")
-                {
-                    if (apiResponse.data.Count == 0)
-                    {
-                        // API returned success, but there's no data
-                        MessageBox.Show("RPAN is currently down, come back tomorrow.");
-                        return;
-                    }
-                    else
-                    {
-                        foreach (RpanData data in apiResponse.data)
-                        {
-                            streams++;
-                            views = views + data.continuous_watchers;
-                            StreamList.Children.Add(new StreamView(data));
-                        }
-                        status.Text = "RPAN: " + streams.ToString() + " streams, " + views.ToString() + " viewers";
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("RPAN API returned with error: " + apiResponse.status);
-                    return;
-                }
+                response = client.GetAsync("videos/seed/360").Result;
             }
+            catch (Exception)
+            {
+                return new ApiResponse { status = "Couldn't connect" };
+            }
+            try
+            {
+                string responseString = response.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<ApiResponse>(responseString);
+            }
+            catch (Exception)
+            {
+                return new ApiResponse { status = "JSON died lol" }; // sorry for this
+            }
+        }
+
+        void parseResponse(ApiResponse response)
+        {
+            int retryLimit = 3;
+            for (int i = 0; i < retryLimit; i++)
+            {
+                if (response.status == "User ID is not found")
+                {
+                    // reddit sometimes returns this
+                    // it seems to get resolved if you retry a lot, idk why
+                    retryLimit = 10;
+                    System.Threading.Thread.Sleep(100);
+                    continue;
+                }
+                if (response.status == "success") // api returned with success (duh)
+                {
+                    tbRefresh.Text = "Click here to refresh";
+
+                    if (response.data.Count == 0) // response contains no streams (usually means that RPAN has ended for today)
+                    {
+                        if (rpanDown.Visibility == Visibility.Visible && tbRpanDown.Text == "RPAN is down") // if already in RpanError, update the header and rotate the pan
+                        {
+                            tbRpanDown.Text = "RPAN is still down";
+                            panRotation += 5;
+                            tbBrokenPan.RenderTransform = new RotateTransform(panRotation);
+                            break;
+                        }
+                        else // if not in RpanDown, reset text and display RpanDown
+                        {
+                            tbRpanDown.Text = "RPAN is down";
+                            rpanDown.Visibility = Visibility.Visible;
+                            break;
+                        }
+                    }
+                    else // there is at least one stream to display
+                    {
+                        tbRefresh.Text = "Listing streams...";
+                        listStreams(response);
+                        break;
+                    }
+
+                }
+                else if (response.status == "Couldn't connect") throwError("Could not connect to RPAN API.");
+                else if (response.status == "JSON died lol") throwError("Could not understand RPAN API response. (Is RPAN fully dead?...)");
+                else throwError("RPAN API returned with error: " + response.status);
+                tbRefresh.Text = "Could not refresh";
+            }
+            // user can click tbRefresh again
+            isRefreshing = false;
+        }
+
+        void throwError(string errorMessage)
+        {
+            MessageBox.Show(errorMessage);
+        }
+
+        void listStreams(ApiResponse response)
+        {
+            foreach (RpanData data in response.data)
+            {
+                streams++;
+                views = views + data.continuous_watchers;
+                StreamList.Children.Add(new StreamView(data));
+            }
+            tbRefresh.Text = "Click here to refresh";
+            rpanDown.Visibility = Visibility.Collapsed;
+            Title = "RpanList - " + streams.ToString() + " streams, " + views.ToString() + " viewers";
+        }
+
+        private void TbRefresh_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (!isRefreshing)
+            {
+                isRefreshing = true;
+                BackgroundWorker refreshbw = new BackgroundWorker();
+                refreshbw.DoWork += new DoWorkEventHandler(_asyncSpeakerThread_DoWork);
+                refreshbw.RunWorkerAsync();
+            }
+        }
+
+        void _asyncSpeakerThread_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() => { tbRefresh.Text = "Refreshing..."; }));
+            Dispatcher.BeginInvoke(new Action(() => { parseResponse(grabResponse()); }));
         }
     }
 }
